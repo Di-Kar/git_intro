6c7eb469c9beb5d03cba0144560efc97d8958adf
Di-Kar <mitry.boss@gmail.com>
2026-08-24 23:40:34 +0700

    Update load_vertica.py

--- ugc_sprint_1/storage-bench/src/load_vertica.py
+++ ugc_sprint_1/storage-bench/src/load_vertica.py
@@ -26,7 +26,7 @@ def main()-> None:
     start = time.perf_counter()
 
     for file_path in files:
-        with open(file_path, "r", encoding="utf-8") as fh:
+        with open(file_path, encoding="utf-8") as fh:
             cur.copy(
                 """
                 COPY bench.events (
